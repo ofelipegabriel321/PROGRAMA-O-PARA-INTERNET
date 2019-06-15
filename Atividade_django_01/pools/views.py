3659de2f7d3bad1e8bb106c2b66b128c471a9b8e
ofelipegabriel <ofelipegabriel321@gmail.com>
2019-06-14 23:28:26 -0300

    Conclusão das questões 3 e 4

--- Atividade_django_01/pools/views.py
+++ Atividade_django_01/pools/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from pools.templates import *
+from .models import *
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def question(request, id_question):
+    question_rendered = Question.objects.get(id=id_question)
+    data = {'question': question_rendered}
+
+    return render(request, 'question.html', data)
