2bd6cd4aee0a1c2dab3432d2092e8cbea1fe9015
ofelipegabriel <ofelipegabriel321@gmail.com>
2019-06-22 15:07:45 -0300

    Atualização do projeto Django 02: inclusão do passo 04, sobre relacionamentos muitos para um

--- Django_slide_02/myapp/models.py
+++ Django_slide_02/myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# PASSO 01, 02 E 03
 class Person(models.Model):
     SHIRT_SIZES = (
         ('S', 'Small'),
@@ -10,4 +11,13 @@ class Person(models.Model):
     name = models.CharField(max_length=30)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
+
+# PASSO 04
+class Manufacter(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Car(models.Model):
+    name = models.CharField(max_length=50)
+    manufacter = models.ForeignKey(Manufacter, on_delete=models.CASCADE, related_name="cars")
 
