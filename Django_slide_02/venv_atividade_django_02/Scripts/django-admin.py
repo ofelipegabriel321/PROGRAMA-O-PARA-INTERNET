e0a916d5d917fd3407f239410e405803ab298fac
ofelipegabriel <ofelipegabriel321@gmail.com>
2019-06-21 23:02:42 -0300

    Iniciação do projeto Django 02 e primeiros passos

---
+++ Django_slide_02/venv_atividade_django_02/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ACER\Desktop\PROGRAMACAO-PARA-INTERNET\Django_slide_02\venv_atividade_django_02\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
