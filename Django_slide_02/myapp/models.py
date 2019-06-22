a47a5f41940126a6354c3e9298e5eef7a08d95e4
ofelipegabriel <ofelipegabriel321@gmail.com>
2019-06-22 17:24:18 -0300

    Atualização do projeto Django 02: inclusão do passo 07, sobre relacionamentos muitos para muitos avançado

--- Django_slide_02/myapp/models.py
+++ Django_slide_02/myapp/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 
 # PASSO 01, 02 E 03
-class Person(models.Model):
-    SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
-    name = models.CharField(max_length=30)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+#class Person(models.Model):
+#    SHIRT_SIZES = (
+#        ('S', 'Small'),
+#        ('M', 'Medium'),
+#        ('L', 'Large'),
+#    )
+#    name = models.CharField(max_length=30)
+#    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
 
 # PASSO 04
@@ -56,4 +57,29 @@ class PessoaFisica(models.Model):
     def __str__(self):
         return self.nome
 
+
+# PASSO 07
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    # PASSO 07.2
+    inviter = models.ForeignKey(Person, on_delete=models.CASCADE, related_name="membership_invites")
+
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
 
